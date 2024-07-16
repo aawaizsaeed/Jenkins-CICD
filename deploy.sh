4164997a65064b2e3eb94d2b4fa2042a53f8af4e
aawaizsaeed <132788999+aawaizsaeed@users.noreply.github.com>
2024-07-16 20:49:48 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,10 +14,10 @@ docker stop ${CONTAINER_NAME} || true
 docker rm ${CONTAINER_NAME} || true
 
 # Run a new container from the pulled image
-docker run -d --name ${CONTAINER_NAME} -p 80:8000 ${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMAGE_TAG}
+docker run -d --name ${CONTAINER_NAME} -p 80:8000 ${IMAGE_NAME}:${IMAGE_TAG}
 
 # Wait for a few seconds to ensure the application is up and running
-sleep 10
+#sleep 10
 
 # Check the application with curl
 curl -f http://localhost:80/ || { echo "Health check failed"; exit 1; }
