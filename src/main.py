be8f693733c07ba98a1e35bc820488635f9d4e46
aawaizsaeed <132788999+aawaizsaeed@users.noreply.github.com>
2024-07-18 11:09:57 +0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! and my name is Aawaiz and roshan is bander nahi hai'
+    return 'Hello, World!'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
